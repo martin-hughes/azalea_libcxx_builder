16f54d599dae3e47654665f7f7cdaf5472431e0f
Martin Hughes <martin-hughes@users.noreply.github.com>
2020-01-23 17:29:36 +0000

    Build libunwind and libcxxabi

--- builder.py
+++ builder.py
@@ -1,12 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.8
 # Main script for azalea_libcxx_builder
 #
-# This scripts builds kernel and user-mode versions of libcxx that are customised for use with Project Azalea.
+# This scripts builds kernel and user-mode versions of libcxx that are customised for use with Project Azalea. It also
+# builds libunwind for use within the kernel.
+#
+# Requires python3.8 or newer.
 
 from contextlib import contextmanager
 import os
 import configparser
 import argparse
+import shutil
 
 def main(config):
   if not os.path.exists("output/kernel_lib"):
@@ -18,38 +22,113 @@ def main(config):
     "-mcmodel=large",
     "-isystem %s" % os.path.abspath("threading_adapter/cxx_include"),
     "-isystem %s" % os.path.abspath(os.path.join(config["PATHS"]["libcxx_base"], "src")),
-    "-isystem %s" % os.path.abspath(os.path.join(config["PATHS"]["kernel_base"], "kernel")),
-    "-isystem %s" % os.path.abspath(os.path.join(config["PATHS"]["kernel_base"], "external", "libcxxrt-master", "src")),
+    "-isystem %s" % os.path.abspath(os.path.join(config["PATHS"]["libcxx_base"], "include")),
+    "-isystem %s" % os.path.abspath(os.path.join(config["PATHS"]["kernel_base"], "kernel"))
     ]
 
-  install_path = os.path.abspath(os.path.join(config["PATHS"]["sys_image_root"], "apps", "developer", "libcxx-kernel"))
+  cxx_install_path = os.path.abspath(os.path.join(config["PATHS"]["sys_image_root"], "apps", "developer", "libcxx-kernel"))
+  unwind_install_path = os.path.abspath(os.path.join(config["PATHS"]["sys_image_root"], "apps", "developer", "libunwind-kernel"))
+  libcxxabi_install_path = os.path.abspath(os.path.join(config["PATHS"]["sys_image_root"], "apps", "developer", "libcxxabi-kernel"))
 
   os.environ["CXXFLAGS"] = " ".join(cxxflags)
+  os.environ["CFLAGS"] = os.environ["CXXFLAGS"]
 
-  print("** In case of errors, ensure LLVM_CONFIG_PATH matches your version of LLVM")
-  cmake_flags = [
+  cmake_libcxx_flags = [
     "cmake",
     os.path.join("..", "..", config["PATHS"]["libcxx_base"]),
-    "-DLLVM_CONFIG_PATH=/usr/lib/llvm-9/bin/llvm-config",
-    "-DCMAKE_INSTALL_PREFIX=%s" % install_path,
+    "-DLLVM_PATH=%s" % config["PATHS"]["llvm_base"],
+    "-DCMAKE_INSTALL_PREFIX=%s" % cxx_install_path,
     "-DLIBCXX_ENABLE_EXCEPTIONS=OFF",
     "-DLIBCXX_ENABLE_SHARED=OFF",
     "-DLIBCXX_ENABLE_STDIN=OFF",
     "-DLIBCXX_ENABLE_STDOUT=OFF",
     "-DLIBCXX_HAS_EXTERNAL_THREAD_API=ON",
     "-DLIBCXX_HAS_PTHREAD_API=OFF",
-    "-DLIBCXX_CXX_ABI=libcxxrt",
+    "-DLIBCXX_CXX_ABI=libcxxabi",
     "-DCMAKE_CXX_COMPILER=/usr/bin/clang++",
     "-DCMAKE_C_COMPILER=/usr/bin/clang",
+    "-DLIBCXX_CXX_ABI_INCLUDE_PATHS=%s" % os.path.join("..", "..", config["PATHS"]["libcxxabi_base"], "include"),
     ]
 
-  with cd("output/kernel_lib"):
-    os.system(" ".join(cmake_flags))
+  cmake_libunwind_flags = [
+    "cmake",
+    os.path.join("..", "..", config["PATHS"]["libunwind_base"]),
+    "-DLLVM_PATH=%s" % config["PATHS"]["llvm_base"],
+    "-DCMAKE_INSTALL_PREFIX=%s" % unwind_install_path,
+    "-DLIBUNWIND_ENABLE_SHARED=OFF",
+    "-DLIBUNWIND_ENABLE_STATIC=ON",
+    "-DCMAKE_CXX_COMPILER=/usr/bin/clang++",
+    "-DCMAKE_C_COMPILER=/usr/bin/clang",
+    "-DLLVM_ENABLE_LIBCXX=ON",
+    "-DLIBUNWIND_ENABLE_ASSERTIONS=OFF",
+    "-DCMAKE_BUILD_TYPE=RELEASE",
+    "-DLIBUNWIND_ENABLE_THREADS=OFF",
+    #"-DLIBUNWIND_C_FLAGS=%s" % ";".join(cxxflags)
+  ]
+
+  cmake_libcxxabi_flags = [
+    "cmake",
+    os.path.join("..", "..", config["PATHS"]["libcxxabi_base"]),
+    "-DLLVM_PATH=%s" % config["PATHS"]["llvm_base"],
+    "-DCMAKE_INSTALL_PREFIX=%s" % libcxxabi_install_path,
+    "-DCMAKE_CXX_COMPILER=/usr/bin/clang++",
+    "-DCMAKE_C_COMPILER=/usr/bin/clang",
+    "-DLIBCXXABI_ENABLE_ASSERTIONS=OFF",
+    "-DCMAKE_BUILD_TYPE=RELEASE",
+    "-DLIBCXXABI_HAS_EXTERNAL_THREAD_API=ON",
+    "-DLIBCXXABI_ENABLE_NEW_DELETE_DEFINITIONS=OFF",
+    "-DLIBCXXABI_ENABLE_SHARED=OFF",
+    "-DLIBCXXABI_ENABLE_STATIC=ON",
+    "-DLIBCXXABI_BAREMETAL=ON",
+    "-DLIBCXXABI_ENABLE_STATIC_UNWINDER=ON",
+    "-DLIBCXXABI_ENABLE_PIC=OFF",
+    "-DLIBCXXABI_LIBCXX_INCLUDE_DIRS=%s" % os.path.join("..", "..", config["PATHS"]["libcxxabi_base"], "include"),
+  ]
+
+  os.makedirs("output/kernel_lib", exist_ok = True)
+  os.makedirs("output/kernel_libunwind", exist_ok = True)
+  os.makedirs("output/kernel_libcxxabi", exist_ok = True)
+
+  print ("--- BUILD LIBCXXABI ---")
+
+  cxxflags = cxxflags + [
+    "-D _LIBUNWIND_IS_BAREMETAL",]
+  os.environ["CXXFLAGS"] = " ".join(cxxflags)
+  os.environ["CFLAGS"] = os.environ["CXXFLAGS"]
+
+  with cd("output/kernel_libcxxabi"):
+    os.system(" ".join(cmake_libcxxabi_flags))
     os.system("make")
     os.system("make install")
 
   os.system("scons sys_image_root='%s' kernel_base='%s'" % (config["PATHS"]["sys_image_root"], os.path.join(config["PATHS"]["kernel_base"])))
   os.system("scons install")
+
+  print ("")
+  print ("--- BUILD LIBUNWIND ---")
+
+  cxxflags = cxxflags + [
+    "-D _LIBUNWIND_IS_BAREMETAL",]
+  os.environ["CXXFLAGS"] = " ".join(cxxflags)
+  os.environ["CFLAGS"] = os.environ["CXXFLAGS"]
+
+  with cd("output/kernel_libunwind"):
+    os.system(" ".join(cmake_libunwind_flags))
+    os.system("make")
+    os.system("make install")
+
+    # For some reason this doesn't install the headers, so copy them manually
+    os.makedirs(os.path.join(unwind_install_path, "include"), exist_ok = True)
+    shutil.copytree(os.path.join("..", "..", config["PATHS"]["libunwind_base"], "include"),
+                    os.path.join(unwind_install_path, "include"),
+                    dirs_exist_ok = True)
+
+  print ("")
+  print ("--- BUILD LIBCXX ---")
+  with cd("output/kernel_lib"):
+    os.system(" ".join(cmake_libcxx_flags))
+    os.system("make")
+    os.system("make install")
 
 # An easy way to wrap CD calls so they get reverted upon exiting or exceptions.
 #
@@ -75,10 +154,13 @@ def regenerate_config(config, cmd_line_args):
 
   populate_field(paths, args_dict, "kernel_base", "Kernel source base directory")
   populate_field(paths, args_dict, "libcxx_base", "LLVM Libc++ base directory")
+  populate_field(paths, args_dict, "libunwind_base", "LLVM Libunwind base directory")
+  populate_field(paths, args_dict, "libcxxabi_base", "LLVM Libcxxabi base directory")
   populate_field(paths, args_dict, "sys_image_root", "Azalea system image root directory")
+  populate_field(paths, args_dict, "llvm_base", "LLVM project llvm source folder")
 
 def populate_field(config_section, args_dict, cfg_name, human_name):
-  if args_dict[cfg_name]:
+  if (cfg_name in args_dict) and args_dict[cfg_name]:
     config_section[cfg_name] = args_dict[cfg_name]
   if cfg_name not in config_section:
     config_section[cfg_name] = prompt_for_value(human_name)
@@ -92,8 +174,11 @@ if __name__ == "__main__":
     argp = argparse.ArgumentParser(description = "Project Azalea Builder helper")
     argp.add_argument("--kernel_base", type = str, help = "Location of the base of the Azalea kernel source code tree")
     argp.add_argument("--libcxx_base", type = str, help = "Location of LLVM Libc++ source code tree")
+    argp.add_argument("--libcxxabi_base", type = str, help = "Location of LLVM Libcxxabi source code tree")
+    argp.add_argument("--libunwind_base", type = str, help = "Location of LLVM Libunwind source code tree")
     argp.add_argument("--sys_image_root", type = str, help = "Root of the Azalea system image's filesystem")
     argp.add_argument("--config_file", type = str, default = "config/saved_config.ini", help = "Config file location")
+    argp.add_argument("--llvm_base", type = str, help = "Location of llvm-project's llvm folder")
     args = argp.parse_args()
 
     flags = "r" if os.path.exists(args.config_file) else "a+"
--- threading_adapter/threading_adapter.cpp
+++ threading_adapter/threading_adapter.cpp
@@ -6,6 +6,8 @@
 #include <errno.h>
 #include "cxx_include/__external_threading"
 #include "processor/processor.h"
+#include "klib/synch/kernel_locks.h"
+#include <atomic>
 
 _LIBCPP_BEGIN_NAMESPACE_STD
 
@@ -59,6 +61,100 @@ int __libcpp_mutex_unlock(__libcpp_mutex_t *__m)
 void __libcpp_thread_yield()
 {
   task_yield();
+}
+
+int __libcpp_execute_once(__libcpp_exec_once_flag *flag,
+                          void (*init_routine)(void))
+{
+  // There are three valid states for *flag:
+  // 0 - No thread has started initialization yet.
+  // 1 - init_routine is being executed, so the caller should wait.
+  // 2 - init_routine has been completed. However, it may have only just been completed, so add a memory barrier to be
+  //     sure.
+  __libcpp_exec_once_flag expected = 0;
+  __libcpp_exec_once_flag desired = 1;
+  if (__atomic_compare_exchange_2(flag, &expected, desired, false, memory_order_acq_rel, memory_order_acquire))
+  {
+    // flag was zero, is now one.
+    init_routine();
+  }
+  else
+  {
+    switch(expected)
+    {
+    case 1:
+      // while (*flag != 2)...
+      expected = 2;
+      desired = 3;
+      while (!__atomic_compare_exchange_2(flag,
+                                          &expected,
+                                          desired,
+                                          false,
+                                          memory_order_acq_rel,
+                                          memory_order_acquire))
+      { };
+      *flag = 2;
+      break;
+
+    case 2:
+      // Thread init already completed.
+      break;
+
+    default:
+      abort();
+    }
+  }
+
+  // In all cases, make sure changes are visible to this thread. This isn't strictly necessary if this thread was the
+  // one doing the initializing.
+  *flag = 2;
+  std::atomic_thread_fence(memory_order_acq_rel);
+
+  return 0;
+}
+
+namespace
+{
+  kernel_spinlock key_array_lock{0};
+  void (*volatile tls_keys[task_thread::MAX_TLS_KEY])(void *){0};
+}
+
+int __libcpp_tls_create(__libcpp_tls_key* __key,
+                        void(_LIBCPP_TLS_DESTRUCTOR_CC* __at_exit)(void*))
+{
+  klib_synch_spinlock_lock(key_array_lock);
+  for (int i = 0; i < task_thread::MAX_TLS_KEY; i++)
+  {
+    if (tls_keys[i] == nullptr)
+    {
+      // Found a key
+      if (__at_exit)
+      {
+        tls_keys[i] = __at_exit;
+      }
+      else
+      {
+        tls_keys[i] = reinterpret_cast<void (*)(void *)>(1);
+      }
+      *__key = i;
+      klib_synch_spinlock_unlock(key_array_lock);
+      return 0;
+    }
+  }
+  klib_synch_spinlock_unlock(key_array_lock);
+
+  return EAGAIN;
+}
+
+void *__libcpp_tls_get(__libcpp_tls_key __key)
+{
+  return task_get_cur_thread()->thread_local_storage_slot[__key];
+}
+
+int __libcpp_tls_set(__libcpp_tls_key __key, void *__p)
+{
+  task_get_cur_thread()->thread_local_storage_slot[__key] = __p;
+  return 0;
 }
 
 _LIBCPP_END_NAMESPACE_STD
